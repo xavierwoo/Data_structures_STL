7dadf8252fe076b42885f1797d55279904c14cb3
xavierwoo <xavier.woo@gmail.com>
2024-04-02 14:59:53 +0800

    Initial Commit

---
+++ data_structures_stl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data_structures_stl
+//
+//  Created by 吴 歆韵 on 2024/4/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
